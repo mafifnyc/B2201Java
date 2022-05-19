46f0d50701d6d866321c820c74e3941198889336
jahidul2543 <jahidul2543@gmail.com>
2022-05-18 22:11:52 -0400

    first commit Java 101

---
+++ src/MyApp.java
@@ -0,0 +1,24 @@
+public class MyApp {
+
+    public static void main(String args[]){
+        System.out.println("I am going to learn Data Types in Java");
+        // Java is a strongly typed language
+        //  Every data in Java must have a type
+        int a = 2;
+        int b = 3;
+        int sum = a + b; // 5 --> Value 5 is what type?
+
+        System.out.println(sum);
+
+        boolean areYouGoodBoy  = false;
+        boolean areYouBadBoy = true;
+
+        char examGrade = 'A';
+
+        String myGrade =  "A";
+
+        System.out.println("My Grade: " + examGrade);
+
+    }
+
+}
